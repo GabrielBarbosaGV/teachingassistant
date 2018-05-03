9672de4d8006f41bccb7394f7559547c45d1fcf5
GabrielBarbosaGV <gbgv@cin.ufpe.br>
2018-05-02 21:09:17 -0300

    Módulo Angular atualizado com novo componente e rota de RouterModule.

--- gui/ta-gui/src/app/app.module.ts
+++ gui/ta-gui/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { MetasComponent } from './metas.component';
 import { AlunosComponent } from './alunos.component';
 import { AlunoService } from './aluno.service';
+import { DiscrepanciasComponent } from './discrepancias/discrepancias.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MetasComponent,
-    AlunosComponent
+    AlunosComponent,
+    DiscrepanciasComponent
   ],
   imports: [
     BrowserModule,
@@ -27,6 +29,10 @@ import { AlunoService } from './aluno.service';
       {
         path: 'alunos',
         component: AlunosComponent
+      },
+      {
+        path: 'discrepancias',
+        component: DiscrepanciasComponent
       }
     ])
   ],
